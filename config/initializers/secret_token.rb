144db928c93171d558edbf3d219934924690e675
Sebastiaan Hoejenbos <seb@Mac.local>
2011-08-08 20:28:42 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Planb::Application.config.secret_token = 'd6631e4b61405039eff7bbf4d24de8b2dfde8ae5a30ccdca76e0de6db343b931d47cf259fa4208a9f422ad6dc56a0fae3b2b5fcd3d73dd446d098463df0bf94a'
