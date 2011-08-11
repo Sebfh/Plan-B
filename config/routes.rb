ec487a5bc440252835f9533accc79881d7ace48b
Michiel Peters <wishjuh@gmail.com>
2011-08-11 17:40:00 +0200

    Login deels af
    login ziet er vaag uit in de browser/iPad emulator maar werkt in de
    iPhone emulator (needs fix i guess) routes changed naar login login
    styles in application.css

--- config/routes.rb
+++ config/routes.rb
@@ -50,7 +50,7 @@ Planb::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-root :to => "pages#welcome"
+root :to => "pages#login"
 
   # See how all your routes lay out with "rake routes"
 
