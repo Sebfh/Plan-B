66022aacaeffeb85f94aea90de4bfb9477e23128
Sebastiaan Hoejenbos <seb@socialr.nl>
2011-08-19 19:30:46 +0200

    Changed the authorization to Touch

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, '7iHQShm7mIMfzTeLXIEA', 'weoyYf9vWDKRJEGsIfsUaCXRbZegxHj80oIFCFXMawA'
-  provider :facebook, '156284807785608', '34fc559abc6c4ec51209c6df440ff500', :scope => 'email'
+  provider :facebook, '156284807785608', '34fc559abc6c4ec51209c6df440ff500', :display => 'touch',:scope => 'email'
 end
