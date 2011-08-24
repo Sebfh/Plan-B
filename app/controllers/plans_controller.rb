c11a20d4e4bf67c7d82094f1272eac5ec00d315f
Sebastiaan Hoejenbos <seb@socialr.nl>
2011-08-24 21:08:29 +0200

    Added action:new to plans

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,3 +1,12 @@
 class PlansController < ApplicationController
-
+  # GET /plans/new
+  # GET /plans/new.xml
+  def new
+    @plan = Plan.new
+    @plan.user = current_user
+    
+    respond_to do |format|
+      format.html # new.html.erb
+    end
+  end
 end
