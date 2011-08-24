5fd0610e550185a033e64fc9cb85b640de32812a
Sebastiaan Hoejenbos <seb@socialr.nl>
2011-08-24 21:23:09 +0200

    Added current_user to the plan

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -13,10 +13,11 @@ class PlansController < ApplicationController
 
   def create
     @plan = Plan.new(params[:plan])
-
+    @plan.user = current_user
+    
     respond_to do |format|
       if @plan.save
-        format.html { redirect_to(@plan, :notice => 'Plan was successfully created.') }
+        format.html { redirect_to root_path, :notice => 'Plan was successfully created.' }
       else
         format.html { render :action => "new" }
       end
