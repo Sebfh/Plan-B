fc2e91177da9c1be60d05736048d55a40d7e64c3
Michiel Peters <wishjuh@gmail.com>
2011-08-26 16:19:57 +0200

    Volgorde datums ascending

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,6 @@ class UsersController < ApplicationController
   end
   def dashboard
     # alle plannen van de current_user ophalen en klaarzetten voor de view
-    @plans = Plan.where(:user_id => current_user)
+    @plans = Plan.where(:user_id => current_user).ascending
   end
 end
--- app/models/plan.rb
+++ app/models/plan.rb
@@ -2,4 +2,17 @@ class Plan < ActiveRecord::Base
   belongs_to :user
   has_many :reactions
   has_one :location
+  
+  validates_presence_of :title, :description, :starts_at, :user_id
+  before_save :cleanup
+  
+  named_scope :descending, :order => "starts_at DESC"
+  named_scope :ascending, :order => "starts_at ASC"
+  
+  private
+  
+  def cleanup
+      self[:title] = self[:title].capitalize
+      self[:description] = self[:description].capitalize      
+  end
 end
