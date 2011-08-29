9f723983ef3ddc7830658f64ae5e3113b1050dc2
Michiel Peters <wishjuh@gmail.com>
2011-08-29 16:33:39 +0200

    When user is destroyed plans and reactions will be destroyed as well
    Added ', :dependent => :destroy' in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
-    has_many :plans
-    has_many :reactions
+    has_many :plans, :dependent => :destroy
+    has_many :reactions, :dependent => :destroy
     def self.create_with_omniauth(auth)
       create! do |user| #een block aan create! hangen kan je bij de user voordat ie in de db word opgeslagen.
         user.provider = auth["provider"]
